a3e1a46258c1eb8e6a5540cff3610bba93bc76c8
Harold Ulloa Torres <48334476+haroldut@users.noreply.github.com>
2023-01-06 23:10:20 +0000

    Pending changes exported from your codespace

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+test("Operations", () => {
+  expect(2+2).toBe(4);
+  expect(3-1).toBe(2);
+  expect(1*3).toBe(3);
+  expect(0/0).toBe(NaN);
+});
+  
+test("Equal", () => {
+  const data = { name: "UT", age: 27, zodiac: "aries" };
+  expect(data).toEqual ({ name: "UT", age: 27, zodiac: "aries" });
+  expect(data).toStrictEqual({ name: "UT", age: 27, zodiac: "aries" });
+});
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Null", () => {
+  const u = undefined;
+  expect(u).toBeUndefined();
+  expect(u).not.toBeNull();
+  expect(u).toBeFalsy();
+});
+
+test("Than", () => {
+  expect(0.9999).toBeGreaterThan(0.99);
+  expect(0.999).toBe(0.999);
+  expect(0.99).toBeLessThan(0.9999);
+});
+
+test("contains", () => {
+  const data = ['a', 'e', 'i', 'o', 'u'];
+  expect(data).toContain('i');
+  expect(data).not.toContain('c');
 });
